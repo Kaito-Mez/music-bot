1f5fd08e603513c6b0a703c265e98a77b02e980a
James-McClure <s3898327@student.rmit.edu.au>
2022-06-17 23:28:22 +1000

    timing adjustments

--- bot.py
+++ bot.py
@@ -149,7 +149,7 @@ class MusicBot(discord.Client):
         if not server.vc:
             if server.is_member_connected(member):
                 await server.join_channel(member.voice.channel.id)
-                await asyncio.sleep(4)
+                await asyncio.sleep(5)
                 await server.play_audio()
 
 
